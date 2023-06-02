34706897afbed1ba715ad99b11112c790fbf1bd3
start.vaadin.com <no-reply@vaadin.com>
2023-06-02 13:11:21 +0000

    Generated project

---
+++ src/main/java/com/awesomepark/app/data/service/SampleAddressRepository.java
@@ -0,0 +1,12 @@
+package com.awesomepark.app.data.service;
+
+import com.awesomepark.app.data.entity.SampleAddress;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface SampleAddressRepository
+        extends
+            JpaRepository<SampleAddress, Long>,
+            JpaSpecificationExecutor<SampleAddress> {
+
+}
